8a62f458c2ba7823b96ef4e227b86684cd786f0e
Anonymous <abc@kata.com>
2020-09-03 10:28:24 +0530

    Add: Start new tennis game with two players

--- src/main/java/com/bnppf/kata/Tennis.java
+++ src/main/java/com/bnppf/kata/Tennis.java
@@ -1,4 +1,19 @@
 package com.bnppf.kata;
 
 public class Tennis {
+    private final String firstPlayerName;
+    private final String secondPlayerName;
+
+    public Tennis(String firstPlayerName , String secondPlayerName) {
+        this.firstPlayerName = firstPlayerName;
+        this.secondPlayerName = secondPlayerName;
+    }
+
+    public String getFirstPlayerName() {
+        return firstPlayerName;
+    }
+
+    public String getSecondPlayerName() {
+        return secondPlayerName;
+    }
 }
