f035ba4588a906c991852c04210c2cc3d759264d
Anonymous <abc@kata.com>
2020-09-03 15:29:17 +0530

    Refactor: Extracted separate method to start game with two valid players

--- src/main/java/com/bnppf/kata/TennisSimulator.java
+++ src/main/java/com/bnppf/kata/TennisSimulator.java
@@ -1,11 +1,20 @@
 package com.bnppf.kata;
 
+import com.bnppf.kata.game.Tennis;
+import com.bnppf.kata.interfaces.TennisInterface;
+import com.bnppf.kata.models.TennisPlayer;
+
 import java.util.Scanner;
 
 public class TennisSimulator {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        TennisInterface tennis = startGameWithTwoPlayers();
+        scanner.close();
+    }
+
+    private static TennisInterface startGameWithTwoPlayers() {
         String firstPlayer;
         String secondPlayer;
         do {
@@ -16,6 +25,6 @@ public class TennisSimulator {
             System.out.print("Player 2 --> ");
             secondPlayer = scanner.nextLine().trim();
         } while ("".equals(firstPlayer) || "".equals(secondPlayer) || firstPlayer.equalsIgnoreCase(secondPlayer));
-        scanner.close();
+        return new Tennis(new TennisPlayer(firstPlayer) , new TennisPlayer(secondPlayer));
     }
 }
