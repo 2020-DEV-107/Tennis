9c269fd0780b69660f53bc6bd8e98cd6b2ea65ad
Anonymous <abc@kata.com>
2020-09-03 15:24:06 +0530

    Refactor: Throw an exception for invalid tennis point

--- src/main/java/com/bnppf/kata/constants/TennisConstants.java
+++ src/main/java/com/bnppf/kata/constants/TennisConstants.java
@@ -8,6 +8,8 @@ public class TennisConstants {
     public static final String TEXT_ADVANTAGE = "Advantage";
     public static final String TEXT_WINNER = "Winner";
     public static final String TEXT_INVALID_PLAYER = "Invalid Player Name";
+    public static final String TEXT_INVALID_POINT = "Invalid Tennis Point";
+    public static final int POINT_ZERO = 0;
     public static final int POINT_ONE = 1;
     public static final int POINT_TWO = 2;
     public static final int POINT_THREE = 3;
--- src/main/java/com/bnppf/kata/enums/TennisScoreEnum.java
+++ src/main/java/com/bnppf/kata/enums/TennisScoreEnum.java
@@ -1,9 +1,14 @@
 package com.bnppf.kata.enums;
 
+import com.bnppf.kata.constants.TennisConstants;
+import com.bnppf.kata.exceptions.TennisException;
+import org.apache.log4j.Logger;
+
 import java.util.Arrays;
 
 public enum TennisScoreEnum {
     LOVE("Love" , 0), FIFTEEN("Fifteen" , 1), THIRTY("Thirty" , 2), FORTY("Forty" , 3);
+    private static final Logger logger = Logger.getLogger(TennisScoreEnum.class);
     private final String score;
     private final int point;
 
@@ -13,6 +18,10 @@ public enum TennisScoreEnum {
     }
 
     public static TennisScoreEnum getScoreFromPoints(int point) {
+        if (point < TennisConstants.POINT_ZERO || point > TennisConstants.POINT_THREE) {
+            logger.error(TennisConstants.TEXT_INVALID_POINT);
+            throw new TennisException(TennisConstants.TEXT_INVALID_POINT);
+        }
         return Arrays.stream(TennisScoreEnum.values()).
                 filter(tennisScore -> tennisScore.point == point).findFirst().orElse(null);
     }
