9601b7bdf8ba324efb670ef6a10e8b45c90595c6
Anonymous <abc@kata.com>
2020-09-03 15:26:54 +0530

    Add: Start playing Tennis Game

---
+++ src/main/java/com/bnppf/kata/TennisSimulator.java
@@ -0,0 +1,21 @@
+package com.bnppf.kata;
+
+import java.util.Scanner;
+
+public class TennisSimulator {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String firstPlayer;
+        String secondPlayer;
+        do {
+            System.out.println("Kindly enter two player names to start the game.");
+            System.out.println("(Player names cannot be same, empty and null.)");
+            System.out.print("Player 1 --> ");
+            firstPlayer = scanner.nextLine().trim();
+            System.out.print("Player 2 --> ");
+            secondPlayer = scanner.nextLine().trim();
+        } while ("".equals(firstPlayer) || "".equals(secondPlayer) || firstPlayer.equalsIgnoreCase(secondPlayer));
+        scanner.close();
+    }
+}
